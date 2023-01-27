7823b1b76210e7f1b03b5487a6cd4c8863dae92e
Vinay Rao <vrao8912@gmail.com>
2023-01-27 19:49:02 +0100

    modified recipemodel

--- models/recipemodel.js
+++ models/recipemodel.js
@@ -5,154 +5,162 @@ const Schema = mongoose.Schema;
 //pass in object for the schema
 const recipeSchema = new Schema({
   title: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  batchVolume: {
-    type: Number,
-    required: true
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    batchVolume: {
+      type: Number,
+      required: true,
+    },
   },
   description: {
     text: {
       type: String,
-      required: true
+      required: true,
     },
     originalGravity: {
       type: Number,
-      required: true
+      required: true,
     },
     bitterness: {
       type: Number,
-      required: true
+      required: true,
     },
     color: {
       type: Number,
-      required: true
+      required: true,
     },
     alcohol: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
   brewingWater: {
     mainInfusion: {
       type: Number,
-      required: true
+      required: true,
     },
     sparge: {
       type: Number,
-      required: true
+      required: true,
     },
     total: {
       type: Number,
-      required: true
-    }
-  },
-  mashGrains: [{
-    grainType: {
-      type: String,
-      required: true
+      required: true,
     },
-    amount: {
-      type: Number,
-      required: true
-    }
-  }],
+  },
+  mashGrains: [
+    {
+      grainType: {
+        type: String,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
   mashSchedule: {
     mashIn: {
       type: Number,
-      required: true
+      required: true,
     },
     rest1: {
       temperature: {
         type: Number,
-        required: true
+        required: true,
       },
       time: {
         type: Number,
-        required: true
-      }
+        required: true,
+      },
     },
     rest2: {
       temperature: {
         type: Number,
-        required: true
+        required: true,
       },
       time: {
         type: Number,
-        required: true
-      }
+        required: true,
+      },
     },
     rest3: {
       temperature: {
         type: Number,
-        required: false
+        required: false,
       },
       time: {
         type: Number,
-        required: false
-      }
+        required: false,
+      },
     },
     mashOut: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
   boil: {
     time: {
       type: Number,
-      required: true
+      required: true,
     },
-    hops: [{
-      type: {
-        type: String,
-        required: true
+    hops: [
+      {
+        type: {
+          type: String,
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+        },
+        boilingTime: {
+          type: Number,
+          required: true,
+        },
       },
-      amount: {
-        type: Number,
-        required: true
+    ],
+    addOns: [
+      {
+        type: {
+          type: String,
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+        },
+        boilingTime: {
+          type: Number,
+          required: true,
+        },
       },
-      boilingTime: {
-        type: Number,
-        required: true
-      }
-    }],
-    addOns: [{
-      type: {
-        type: String,
-        required: true
-      },
-      amount: {
-        type: Number,
-        required: true
-      },
-      boilingTime: {
-        type: Number,
-        required: true
-      }
-    }]
+    ],
   },
   fermentation: {
     yeast: {
       type: String,
-      required: true
+      required: true,
     },
     fermentationTemperature: {
       type: Number,
-      required: true
+      required: true,
     },
     finalGravity: {
       type: Number,
-      required: true
+      required: true,
     },
     carbonation: {
       type: Number,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 });
 module.exports = mongoose.model("Recipe", recipeSchema);
